7ab5e3d15aee817d844045d757aa2a7421ee3455
FrancoBondioni <bondionifranco@gmail.com>
2024-01-06 19:15:20 -0300

    desafio-2

---
+++ Desafio-2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FechaValidaComponent from './components/FechaValidaComponent'
+
+function App() {
+
+  return (
+    <>
+      <FechaValidaComponent />
+    </>
+  )
+}
+
+export default App
